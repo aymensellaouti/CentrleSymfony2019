1c9e954d24a0a298e62ba37f13c17795503b68eb
aymen <aymen.sellaouti@gmail.com>
2019-10-29 17:26:06 +0100

    =todo add

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoController extends AbstractController
@@ -30,5 +31,36 @@ class TodoController extends AbstractController
             $session->getFlashBag()->add('info', 'Session initialisée avec succès :)');
         }
         return $this->render('todo/index.html.twig');
+    }
+
+    /**
+     * @param Request $request
+     * @Route("/todo/add/{name}/{content}", name="todo.add")
+     */
+    public function add(Request $request, $name, $content) {
+        $session = $request->getSession();
+        if(!$session->has('todos')) {
+            $session->getFlashBag()->add('error', 'Session non encore initialisée :(');
+        } else {
+            $todos = $session->get('todos');
+            if(isset($todos[$name])){
+                $session->getFlashBag()->add('error', " le todo de clé $name existe déjà :(");
+            } else {
+                $todos[$name]=$content;
+                $session->set('todos', $todos);
+                $session->getFlashBag()->add('success', "Le Todo $name a été ajouté avec suuccès :)");
+            }
+        }
+        return $this->redirectToRoute('todo');
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route("/todo/vider", name="todo.vider")
+     */
+    public function viderSession(Request $request) {
+        $request->getSession()->clear();
+        return new Response("<h1>Session vidée</h>");
     }
 }
