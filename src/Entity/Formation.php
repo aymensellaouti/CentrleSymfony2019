1ecfdefd75982b0ec249fe4a433c7e11beac50a2
aymen <aymen.sellaouti@gmail.com>
2019-12-14 08:58:36 +0100

    Gestion des formulaires 2

--- src/Entity/Formation.php
+++ src/Entity/Formation.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -10,6 +11,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\FormationRepository")
  * @ORM\HasLifecycleCallbacks()
+ * @ApiResource()
  */
 class Formation
 {
